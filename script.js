ee6a8a3d8d658ae83b657db58aa270d5346a5791
Silvia <silviaelen.developer@gmail.com>
2024-11-09 13:26:03 -0200

    Implementação das functions responsaveis por salvar e apresentar dados que houverem; Iniciando função getLenders que apresenta links de financeiras.'

--- script.js
+++ script.js
@@ -28,16 +28,62 @@
         save(amount.value, apr.value, years.value, zipcode.value);
 
         //Anuncio: localiza e exibe financeira locais, mas ignora erros de rede
-        try{
-            getLenders(amount.value, apr.value, yeats.value, zipcode.value)
-        }catch(e){
-            chart(principal, interest, monthly, payments);
-        }
-        
-    }else{
-        payment.innerHTML = '';
+
+        try{//Capututa quais que ocorram dentro dessas chaves
+            getLenders(amount.value, apr.value, years.value, zipcode.value)
+        }catch(e){} //Ignora os erros
+
+        chart(principal, interest, monthly, payments); //Traça o grafico do saldo devedor, dos juros e do pagamento do capital
+
+    } else {//Caso o valor não seja um numero ou seja infinito, todas as saidas ficam vazias
+        payment.innerHTML = ''; 
         total.innerHTML = '';
         totalinterest.innerHTML = '';
-        chart();
+        chart();//chart(), sem argumentos, apaga o grafico.
     }
-}
+};
+
+//Salva a entrada do usuario como propriedades do objeto localStorage.
+
+function save(amount, apr, years, zipcode){
+    if(window.localStorage){ //Faz somente se o navegador suportar
+        localStorage.loan_amount = amount;
+        localStorage.loan_apr = apr;
+        localStorage.loan_years = years;
+        localStorage.loan_zipcode = zipcode;
+    }
+};
+
+//Tenta restaurar os campos de entrada automaticamente quando o documento é carregado
+
+window.onload = function(){
+    if(window.localStorage && localStorage.loan_amount){ //Se o navegador suporta e temos dados, restaure-os;
+        document.querySelector('#amount').value = localStorage.loan_amount;
+        document.querySelector('#apr').value = localStorage.loan_apr;
+        document.querySelector('#years').value = localStorage.loan_years;
+        document.querySelector('#zipcode').value = localStorage.loan_zipcode;
+    }
+};
+
+//Passa a entrada do usuario para um script no lado do servidor que teoricamente pode retornar.
+//OBS: NÃO A IMPLEMENTAÇÃO REAL DESSE SERVIÇO (LOGO NÃO HÁ RETORNO)
+
+function getLenders(amount, apr, years, zipcode){
+    if(window.XMLHttpRequest) return; //Se o navegador não suportar não faça nada
+    
+    var ad = document.querySelector('#lenders');
+
+    if(!ad) return;
+
+    //Codifca a entrada do user como parametro de consulta em um Url
+    var url = 'getLendrs.php' +
+              '?amt=' + encodeURIComponent(amount) +
+              '&apr=' + encodeURIComponent(apr) +
+              '&yrs=' + encodeURIComponent(years) +
+              '&zip=' + encodeURIComponent(zipcode);
+
+    //Busca o o conteudo do Url criado usando um objeto XMLHTpRequest
+    var req = new XMLHttpRequest(); //Inicia um novo pedido
+    req.open('GET',url);            //Pedido GET da HTTP para o Url
+    req.send(null);                 //Envia o pedido sem corpo
+};
