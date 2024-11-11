5f7e595bf76da4a8f7881429499436e9f2bcdbe6
Silvia <silviaelen.developer@gmail.com>
2024-11-11 07:12:29 -0200

    Concluindo Projeto

--- script.js
+++ script.js
@@ -94,9 +94,94 @@ function getLenders(amount, apr, years, zipcode){
                                                        // 'status' é o codigo HTTP da resposta 200 = Requisição bem sucedida
             var response = req.responseText; //Texto da resposta do servidor
             var lenders = JSON.parse(response); // Converte a string JSON da resposta em um obj JS.
-        }else{
-            
-        }
-    };
-};
+        
+            var list = '';
+            for(var i = 0; i < lenders.length; i++){
+            list += "<li><a href='" + lenders[i].url + "'>" +
+            lenders[i].name + "</a>";
+            }
 
+            ad.innerHTML = "<ul>" + list + "</ul>";
+        
+        }
+    }
+}
+
+
+function chart(principal, interest, monthly, payments){
+    var graph = document.querySelector('#graph');
+    graph.width = graph.width;
+
+    if(arguments.length == 0 || !graph.getContext) return;
+
+    var g = graph.getContext('2d');
+    var width = graph.width, height = graph.height;
+
+    function paymentToX (n) {return n * width/payments;}
+    function amountToY (a) {return height - (a * height/(monthly*payments*1.05));}
+
+    g.moveTo(paymentToX(0), amountToY(0));
+    g.lineTo(paymentToX(payments),
+             amountToY(monthly*payments));
+    g.lineTo(paymentToX(payments), amountToY(0));
+
+    g.closePath();
+    g.fillStyle = '#f88';
+    g.fill();
+    g.font = 'bold 12ppx sans-serif';
+    g.fillText('Total Interest Payments', 20, 20);
+
+    var equity = 0;
+    g.beginPath();
+    g.moveTo(paymentToX(0), amountToY(0));
+
+    for(var p = 1; p <= payments; p++){
+        var thisMonthsInterst = (principal-equity)*interest;
+        equity += (monthly - thisMonthsInterest);
+        g.lineTo(paymentToX(p),amountToY(equity));
+    }
+
+    g.lineTo(paymentToX(payments), amountToY(0));
+    g.closePath();
+    g.fillStyle = "green";
+    g.fill();
+    g.fillText("Total Equity", 20,35);
+
+    var bal = principal;
+    g.beginPath();
+    g.moveTo(paymentToX(0),amountToY(bal));
+    for(var p = 1; p <= payments; p++) {
+        var thisMonthsInterest = bal*interest;
+        bal -= (monthly - thisMonthsInterest);
+        g.lineTo(paymentToX(p),amountToY(bal));
+    }
+    
+    g.lineWidth = 3;
+    g.stronke();
+    g.fillStyle = 'black';
+    g.fillText("Loan Balance",20,50);
+
+    g.TextAlign = "center";
+
+    var y = amountToY(0);
+    for(var year=1; year*12 <=payments; year++){
+        var x = paymentToX(year * 12);
+        g.fillRect(x-0.5,y-3,1,3);
+
+        if(year ==1) g.fillText('Year',x,y-5);
+
+        if(year % 5 == 0 && year*12 !== payments)
+            g.fillText(String(year), x, y-5);
+    }
+
+    g.TextAlign = "right";
+    g.textBaseline = "middle";
+    var ticks = [monthly*payments, principal];
+    var rightEdge = paymentToX(payments); 
+    for(var i = 0; i < ticks.length; i++) { 
+        var y = amountToY(ticks[i]); 
+        g.fillRect(rightEdge-3, y-0.5, 3,1); 
+        g.fillText(String(ticks[i].toFixed(0)), 
+        rightEdge-5, y);
+    }
+}
